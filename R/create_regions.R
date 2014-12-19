ca6cee661daf710b295d1c43f91c2d2fd9e5e789
welch16 <welch@stat.wisc.edu>
2014-12-19 12:45:12 -0600

    Remove extract reads method

--- R/create_regions.R
+++ R/create_regions.R
@@ -37,51 +37,31 @@ reads_overlaps <- function(chr,chr_islands,chr_reads)
   return(chr_overlaps)
 }
 
-#' Function to convert to data.table
-#'
-#' @param x, A GRanges object
-#'
-#' @return A data.table object
-#'
-#' @export
-data.table.GRanges <- function(x)data.table(as.data.frame(x))
 
-#' Function to convert to data.table
-#'
-#' @param x, A Hits object
-#'
-#' @return A data.table object
-#'
-#' @export
-data.table.Hits <- function(x)data.table(as.data.frame(x))
+
+.data.table.Hits <- function(x)data.table(as.data.frame(x))
 
 
 
-
-#' Extract the reads that overlap an island from the whole reads list for a certain chromosome (or genome)
-#'
-#' @param chr, This is the chromosome (or genome) for which we are looking the regions
-#'
-#' @param chr_reads, GRanges object with a set of reads for certain chromosome (or genome)
-#'
-#' @param chr_overlaps, A Hits object with all the overlaps between the reads and the islands or certain chromosome (or genome)
-#'
-#' @param chr_islands, IRanges object with a collection of regions in certain chromosome (or genome)
-#'
-#' @return A list with all the overlaps between reads and island for each chromosome
-#'
-#' @export
-extract_reads <- function(chr,chr_reads,chr_overlaps,chr_islands)
+.data.table.GRanges <- function(x)
 {
-  chr_overlaps = data.table.Hits(chr_overlaps)
-  chr_reads = data.table.GRanges(chr_reads)
-  setkey(chr_overlaps,queryHits)
-  chr_reads$ID = 1:nrow(chr_reads)
-  setkey(chr_reads,ID)
-  chr_islands = data.table.GRanges(GRanges(seqnames = chr,
-    ranges = ranges(chr_islands),strand = "*"))
-  ov_reads = chr_reads[chr_overlaps[queryHits %in% 1:nrow(chr_islands),list(subjectHits)],]
-  ov_reads[,ID := NULL]
-  ov_reads[,region:=chr_overlaps[,queryHits]]
-  return(ov_reads)
+  dt = data.table(seqnames = as.character( seqnames(x)),
+    start = start(x),end = end(x),
+    strand = as.character(strand(x)))
+  return(dt)
 }
+
+
+# Extract the reads that overlap an island from the whole reads list for a certain chromosome (or genome)
+#
+# @param chr, This is the chromosome (or genome) for which we are looking the regions
+#
+# @param chr_reads, GRanges object with a set of reads for certain chromosome (or genome)
+#
+# @param chr_overlaps, A Hits object with all the overlaps between the reads and the islands or certain chromosome (or genome)
+#
+# @param chr_islands, IRanges object with a collection of regions in certain chromosome (or genome)
+#
+# @return A list with all the overlaps between reads and island for each chromosome
+#
+# @export
