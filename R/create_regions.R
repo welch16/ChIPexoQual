2b1dbe46689a25f31bdb5e4527c400184df36ac1
welch16 <welch@stat.wisc.edu>
2014-12-18 21:44:19 -0600

    Added reads_overlaps function

--- R/create_regions.R
+++ R/create_regions.R
@@ -13,7 +13,25 @@ create_regions <- function(reads,lower)
   stopifnot(class(reads)=="GRanges")
   stopifnot(lower > 0)
   cover = coverage(reads)
-  islands = slice(cover,lower = lower,rangesOnly=TRUE)
+  islands = slice(cover,lower = lower,rangesOnly=FALSE)
   return(islands)
 }
 
+
+
+#' Find the overlaps between the reads and regions for certain chromosome
+#'
+#' @param chr, This is the chromosome (or genome) for which we are looking the regions
+#'
+#' @param chr_islands, IRanges object with a collection of regions in certain chromosome (or genome)
+#'
+#' @param chr_reads, GRanges object with a set of reads for certain chromosome (or genome)
+#'
+#' @export
+
+reads_overlaps <- function(chr,chr_islands,chr_reads)
+{
+  chr_overlaps= findOverlaps(GRanges(seqnames = chr,
+     ranges = chr_islands,strand = "*"),chr_reads)
+  return(chr_overlaps)
+}
