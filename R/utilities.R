7d57e5f8ed66091addb4747fd9954fb5fce69792
welch16 <welch@stat.wisc.edu>
2015-01-12 17:46:35 -0600

    Added functions to create an empty data.table with only the region field and to add variables to it

--- R/utilities.R
+++ R/utilities.R
@@ -9,4 +9,14 @@
   return(dt)
 }
 
+.create_summary <- function(n)return(data.table(region = 1:n))
+.add_variable <- function(summary_table,value,name)
+{
+  summary_table[,(name):=as.integer(0)]
+  setkey(summary_table,region)
+  summary_table[value[,.(region)],(name):=value[,.(V1)]]
+  return(summary_table)
+}
+
+
 reads2IRanges <- function(x)IRanges(start = x$start,end = x$end)
