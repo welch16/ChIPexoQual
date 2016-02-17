dc1381f99d52cf14b6440a27ae735226920ca027
welch16 <welch@stat.wisc.edu>
2016-02-17 13:45:36 -0600

    changed function to create summary to make it clearer and allow create_exo_experiment to calculate the summary statistics in parallel

--- R/create_exo_experiment.R
+++ R/create_exo_experiment.R
@@ -1,19 +1,19 @@
 
 ##'
 ##' @export
-create_exo_experiment <- function(file,height = 1,calc_summary = FALSE)
+create_exo_experiment <- function(file,height = 1,calc_summary = FALSE,parallel = FALSE,mc = 0)
 {
   stopifnot(file.exists(file))
   stopifnot(is.numeric(height))
   stopifnot(is.logical(calc_summary))
-  
+
   rr <- readGAlignments(file,param = NULL)
   rr <- as(rr,"GRanges")
   
   out <- new("ChIPexo_experiment",file = file, reads = rr , depth = length(rr),height = height)
   
   if(calc_summary){
-    out <- separate_regions(out)
+    out <- separate_regions(out,parallel , mc)
   }
   
   return(out)
@@ -22,59 +22,63 @@ create_exo_experiment <- function(file,height = 1,calc_summary = FALSE)
 
 ##'
 ##' @export
-separate_regions <- function(exo_expmt)
-{
+separate_regions <- function(exo_expmt,parallel,mc)
+{  
   exo_reads <- reads(exo_expmt)
   sqnms <- levels(seqnames(exo_reads))
-  exo_reads <- split(exo_reads,as.character(seqnames(exo_reads)))
-  cover <- lapply(exo_reads,coverage)
-  regions <- lapply(cover,slice , lower = height(exo_expmt),rangesOnly = TRUE)
-  regions <- mapply(function(x,y)x[[y]],regions,sqnms,SIMPLIFY = FALSE)
-  regions <- mapply(function(x,y)GRanges(seqnames = x,ranges = y),sqnms,regions,SIMPLIFY = FALSE)
-  regions <- unlist(GRangesList(regions))
-  exo_expmt@summary_stats <- calculate_summary(regions,reads(exo_expmt))
+  cover <- coverage(exo_reads)
+  regions <- slice(cover,lower = height(exo_expmt),rangesOnly = TRUE)
+  regions <- as(regions,"GRanges")
+  if(!parallel){
+    exo_expmt@summary_stats <- calculate_summary(regions,reads(exo_expmt))
+  }else{
+    regions_list <- split(regions,as.character(seqnames(regions)))
+    reads_list <- reads(exo_expmt)
+    reads_list <- split(reads_list,as.character(seqnames(reads_list)))
+    stats_list <- mcmapply(calculate_summary,regions_list,reads_list,SIMPLIFY = FALSE,mc.cores = mc)
+    exo_expmt@summary_stats <- do.call(rbind,stats_list)
+  }  
   return(exo_expmt)
 }
 
 
 
 
-calculate_summary <- function(region,reads,mc = detectCores())
+calculate_summary <- function(region,reads)
 {
   ## fix formats and stuff
-  
   ov <- findOverlaps(region,reads)
   reads <- gr2dt(reads)
   w <- width(region)    
   region <- gr2dt(region)
   region[ , width := w]
+  ##
   region[, match := paste0(seqnames,":",start,"_",end)]
-  reads[  subjectHits(ov), match := region[queryHits(ov), (match)] ]  
-  reads[,strand := ifelse(strand == "+", "F","R")]
+  reads[  subjectHits(ov), match := region[queryHits(ov), (match)] ]
+  ##
   reads <- reads[!is.na(match)]
-
   ## get base statistics
-  f <- reads[,sum(strand == "F"),by = match]
-  setnames(f,names(f),c("match","f"))
-  setkey(f,match)
-  r <- reads[,sum(strand == "R"),by = match]
-  setkey(r,match)
-  setnames(r,names(r),c("match","r"))
-  f_uniq <- reads[strand == "F",length(unique(start)),by = match]
-  setnames(f_uniq,names(f_uniq),c("match","f_pos"))
-  setkey(f_uniq,match)
-  r_uniq <- reads[strand == "R",length(unique(end)),by = match]
-  setnames(r_uniq,names(r_uniq),c("match","r_pos"))
-  setkey(r_uniq,match)
+  fwd <- reads[strand == "+",
+    {
+      f = length(start)
+      f_pos = length(unique(start))
+      list(f = f,f_pos = f_pos)
+    },by = match]
+  bwd <- reads[strand == "-",
+    {
+      r = length(end)
+      r_pos = length(unique(end))
+      list(r = r,r_pos = r_pos)
+    },by = match]
 
   ## merge statistics
-  stats <- merge(region,f,by = "match",allow.cartesian = TRUE)
-  stats <- merge(stats,r,by = "match",allow.cartesian = TRUE)
-  stats <- merge(stats,f_uniq,by = "match",allow.cartesian = TRUE,all = TRUE)
-  stats <- merge(stats,r_uniq,by = "match",allow.cartesian = TRUE, all = TRUE)
+  stats <- merge(region,fwd, by = "match",all.x = TRUE)
+  stats <- merge(stats,bwd, by = "match",all.x = TRUE)
+  stats[is.na(f), f := 0]
+  stats[is.na(r), r := 0]
   stats[is.na(f_pos), f_pos := 0]
   stats[is.na(r_pos), r_pos := 0]
-
+  
   ## calculate composite stats
   stats[ , depth := f + r]
   stats[ , npos := f_pos + r_pos]
@@ -85,8 +89,8 @@ calculate_summary <- function(region,reads,mc = detectCores())
   stats[ , M := as.numeric(NA)]
   stats[ , A := as.numeric(NA)]
 
-  stats[f > 0 & r > 0, M := log2(f * r) - 2 * log2(width)]
-  stats[f > 0 & r > 0, A := log2( f/ r)]
+  stats[f > 0 & r > 0, M := log2(f) + log2(r) - 2 * log2(width)]
+  stats[f > 0 & r > 0, A := log2(f/ r)]
 
   stats[ , strand := NULL]
 
