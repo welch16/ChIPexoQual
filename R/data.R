418dbe2058d1d4e0b5cda5538d08c1ac81128691
welch16 <welch16@gmail.com>
2016-08-08 16:09:47 -0500

    moved the exampleExoData file from ChIPexoQualExample to ChIPexoQual to avoid circular references

---
+++ R/data.R
@@ -0,0 +1,16 @@
+##' \code{ExoData} results for FoxA1 ChIP-exo experiment
+##' 
+##' List of \code{ExoData} objects, generated with \code{ChIPexoQual} and the
+##' files:
+##' 
+##' \itemize{
+##' \item ChIPexo_carroll_FoxA1_mouse_rep1_chr1.bam
+##' \item ChIPexo_carroll_FoxA1_mouse_rep2_chr1.bam
+##' \item ChIPexo_carroll_FoxA1_mouse_rep3_chr1.bam
+##' }
+##' 
+##' @docType data
+##' @name exampleExoData
+##' @usage data(exampleExoData)
+##' @format ExoData object, which are \code{GRanges} with additional columns.
+NULL
